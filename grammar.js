4bdbdea25a42e5aa801a94c2824b0b3d1f1897e0
Mark Skelton <mdskelton99@gmail.com>
2022-12-26 11:23:17 -0600

    Inline interpolation

--- grammar.js
+++ grammar.js
@@ -4,9 +4,20 @@ module.exports = grammar(CSS, {
   name: "styled",
   rules: {
     _top_level_item: ($, original) => choice(original, $.interpolation),
+
     _block_item: ($, original) => choice(original, $.interpolation),
 
+    _value: ($, original) =>
+      prec(
+        -1,
+        choice(
+          ...original.content.members,
+          alias($._inline_interpolation, $.interpolation)
+        )
+      ),
     interpolation: $ => seq("${", $.js_expression, choice("}", "};")),
+
+    _inline_interpolation: $ => seq("${", $.js_expression, "}"),
 
     js_expression: $ => /[A-z_][A-z0-9_]*/,
   },
